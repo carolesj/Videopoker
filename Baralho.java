f4bc9f87be8316d5aac916baf09cc606a42632ae
carolesj <caroline.jesuino.silva@usp.br>
2017-04-04 12:52:26 -0300

    Classes básicas criadas

---
+++ Baralho.java
@@ -0,0 +1,37 @@
+/**
+ * Created by caroles on 04/04/2017.
+ */
+
+public class Baralho {
+    public Baralho () {
+        int i, j;
+        baralho = new Carta [52];
+        //percorre o vetor, dividindo-o em 4 grupos (naipes) e colocando valores numéricos
+        //que representam as cartas. 1 representa A, 11 representa J, 12 representa Q, 13
+        //representa K
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 13; j++) {
+                baralho[i + j] = new Carta(j + 1, i + 1);
+            }
+        }
+    }
+
+    Carta baralho[];
+
+    Random rand = new Random();
+    public tira_mao () {
+        Carta mao[];
+        int posicao;
+        mao = new Carta [5];
+        for (int i = 0; i < 5; i++) {
+            do {
+                posicao = rand.getIntRand(51);
+            } while (baralho[posicao].foi_tirada);
+            mao[i] = baralho[posicao];
+            baralho[posicao].foi_tirada = true;
+        }
+
+    }
+
+
+}
---
+++ Carta.java
@@ -0,0 +1,13 @@
+
+public class Carta extends java.lang.Object{
+
+    public Carta(int numero, int naipe) {
+        this.numero = numero;
+        this.naipe = naipe;
+    }
+
+    int numero;
+    int naipe;
+    boolean foi_tirada = false;
+
+}

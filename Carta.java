d4c258c5e636012e5781b8a9243472be953d6d1c
carolesj <caroline.jesuino.silva@usp.br>
2017-04-04 13:37:52 -0300

    Adicionado método 'toString()'

--- Carta.java
+++ Carta.java
@@ -10,4 +10,29 @@ public class Carta extends java.lang.Object{
     int naipe;
     boolean foi_tirada = false;
 
+    @Override
+    public String toString () {
+        String retorno;
+        if (numero == 1) {
+            retorno = ".A de ";
+        } else if (numero == 11) {
+            retorno = ".J de ";
+        } else if (numero == 12) {
+            retorno = ".Q de ";
+        } else if (numero == 13) {
+            retorno = ".K de ";
+        } else {
+            retorno = ".%d de ";
+        }
+        if (naipe == 1) {
+            retorno += "Paus";
+        } else if (naipe == 2) {
+            retorno += "Ouros";
+        } else if (naipe == 3) {
+            retorno += "Espadas";
+        } else {
+            retorno += "Copas";
+        }
+    }
+
 }

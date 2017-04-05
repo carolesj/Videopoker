b16204060380e5ba53f470e4329ecffe3739947c
carolesj <caroline.jesuino.silva@usp.br>
2017-04-05 17:36:20 -0300

    Finalizadas implementações maiores do jogo

--- Baralho.java
+++ Baralho.java
@@ -57,11 +57,11 @@ public class Baralho {
     }
 
     public int VerificaResultados (Carta[] mao) {
-        int multiplicador, i;
+        int i;
         int [] quantidades;
         quantidades = new int[13];
-        int pares, trinca;
-        boolean quadra;
+        int pares = 0, trinca = 0, n_uns = 0, primeiro_um = 0;
+        boolean quadra = false, eh_flush, eh_straight = false;
         for (i = 0; i < 5; i++) {
             quantidades[mao[i].numero]++;
         }
@@ -76,22 +76,64 @@ public class Baralho {
                 quadra = true;
             }
         }
-        if (pares == 2) {
-            multiplicador = 1;
-        } else if (pares != 1 && trinca == 1) {
-            multiplicador = 2;
-        } else if (pares == 1 && trinca == 1) {
-            multiplicador = 20;
-        } else if (quadra) {
-            multiplicador = 50;
-        } else if () {
+        for (i = 0; i < 13; i++) {
+            if (quantidades[i] == 1) {
+                n_uns++;
+            }
+        }
+        for (i = 1; i < 13; i++) {
+            if (quantidades[i] == 1) {
+                primeiro_um = i;
+                break;
+            }
+        }
 
+
+        /*o código daqui pra baixo verifica as possibilidades de ganhar e retorna o multiplicador da aposta*/
+        //verifica a existência de dois pares
+        if (pares == 2) {
+            return 1;
+        //verifica a existência de uma trinca
+        } else if (pares != 1 && trinca == 1) {
+            return 2;
+        //verifica a existência de um full hand
+        } else if (pares == 1 && trinca == 1) {
+            return 20;
+        //verifica a existência de uma quadra
+        } else if (quadra) {
+            return 50;
+        }
+        eh_flush = (mao[0].naipe == mao[1].naipe && mao[1].naipe == mao[2].naipe && mao[1].naipe == mao[3].naipe &&
+                mao[1].naipe == mao[4].naipe);
+        if (n_uns == 5 && primeiro_um == 10) {
+            //verifica a existência de um straight flush
+            if (eh_flush) {
+                return 200;
+            } else {
+                return 5;
+            }
+        //verifica se existem 5 cartas com valores diferentes e a primeira é menor que 9
+        } else if (n_uns == 5 && primeiro_um <= 9) {
+            //verifica a existência de um straight
+            eh_straight = (quantidades[primeiro_um + 1] + quantidades[primeiro_um + 2] +
+                    quantidades[primeiro_um + 3] + quantidades[primeiro_um + 4] == 4);
+        }
+        //verifica a existência de um straight flush
+        if (eh_straight && eh_flush) {
+            return 100;
+        //verifica a existência de um straight
+        } else if (eh_straight) {
+            return 5;
+        //verifica a existência de um flush
+        } else if (eh_flush) {
+            return 10;
+        //verifica a existência de um nada
+        } else {
+            return 0;
         }
 
 
 
-
-        return multiplicador;
     }
 
 
